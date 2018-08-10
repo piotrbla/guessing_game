3df4c0d7906ae9f4ae5629c1a69be2e7e7d36280
PiotrB <piotrb@gmail.com>
2018-08-10 23:36:27 +0200

    Input error handling

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,15 @@ fn main() {
         let mut guess_buffer = String::new();
         stdin().read_line(&mut guess_buffer)
             .expect("Failed to read line");
-        let guess: u32 = guess_buffer.trim().parse()
-            .expect("Please type a number!");
-
+//        let guess: u32 = guess_buffer.trim().parse()
+//            .expect("Please type a number!");
+        let guess: u32 = match guess_buffer.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please type a number!");
+                continue
+            }
+        };
         println!("You guessed: {}", guess);
         if guess.cmp(&secret_number) == Ordering::Equal {
             println!("Correct");
