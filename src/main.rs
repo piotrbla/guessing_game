0f183d083fe8ddd3e45f1c64fe18d5559a3fbf68
PiotrB <piotrb@gmail.com>
2018-08-10 18:06:12 +0200

    Real guessing

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::io::stdin;
 fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("The secret number is: {}", secret_number);
 
     loop {
         println!("Please input your guess.");
@@ -30,4 +29,5 @@ fn main() {
             }
         }
     }
+    println!("The secret number is: {}", secret_number);
 }
