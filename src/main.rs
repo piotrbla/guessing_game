06f3d79263cc949d5e4af504c2f2d904b9e20a0b
PiotrB <piotrb@gmail.com>
2018-08-10 18:02:43 +0200

    First working version with random number guessing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
+extern crate rand;
+use std::cmp::Ordering;
+use rand::Rng;
 use std::io::stdin;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-//    let mut guess2 : i32;
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_number);
 
-    stdin().read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    println!("You guessed: {}", guess);
+        let mut guess_buffer = String::new();
+        stdin().read_line(&mut guess_buffer)
+            .expect("Failed to read line");
+        let guess: u32 = guess_buffer.trim().parse()
+            .expect("Please type a number!");
+
+        println!("You guessed: {}", guess);
+        if guess.cmp(&secret_number) == Ordering::Equal {
+            println!("Correct");
+        }
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
